443715ceab53e1a291938293397a2b2a2b5068a2
Christoforos <clapathiotis@gmail.com>
2025-02-20 11:03:19 +0100

    connected angular with springboot application

--- library_app/src/main/java/com/library/library_app/LibraryAppApplication.java
+++ library_app/src/main/java/com/library/library_app/LibraryAppApplication.java
@@ -3,6 +3,8 @@ package com.library.library_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,10 +12,16 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableJpaRepositories(basePackages = "com.library.library_app.Repository")
 @EntityScan(basePackages = "com.library.library_app.Entity")
 @ComponentScan(basePackages = "com.library.library_app")
+@RestController
 public class LibraryAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "Hello World";
 	}
 
 }
