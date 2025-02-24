1e91f0233a018d0f2dafca813e16eefe9c8795af
Christoforos <clapathiotis@gmail.com>
2025-02-24 12:01:46 +0100

    Docs added in angular app

--- library_app/src/app/app.module.ts
+++ library_app/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+//// The AppModule class is the root module of the Angular application
+//// It is decorated with the @NgModule decorator
+//// The @NgModule decorator takes a metadata object that tells Angular
+//// how to compile and run the application
+//// The metadata object has the following properties:
+//// declarations: An array of components, directives, and pipes that belong to the module
+//// imports: An array of modules that the module depends on
+//// providers: An array of services that the module provides
+//// bootstrap: The root component that Angular creates and inserts into the index.html file
 @NgModule({
   declarations: [
     AppComponent
--- library_app/src/app/app.routes.ts
+++ library_app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
+/// The routes array is empty
+/// This is because the application does not have any routes
+/// The application is a standalone application
 export const routes: Routes = [];
